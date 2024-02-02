ebed355a940a4e924d2818ec7788bbd80481d4bd
qetzing <general@qetz.de>
2024-02-02 20:55:25 +0100

    Add fifth image and change order of appearance in landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import image1 from '@/images/photos/image-1.jpg'
 import image2 from '@/images/photos/image-2.jpg'
 import image3 from '@/images/photos/image-3.jpg'
 import image4 from '@/images/photos/image-4.jpg'
+import image5 from '@/images/photos/image-5.jpg'
 import {Container} from "@/components/Container";
 import {GitHubIcon, LinkedInIcon} from "@/components/SocialIcons";
 import {Button} from "@/components/Button";
@@ -131,7 +132,7 @@ function Photos() {
   return (
     <div className="mt-16 sm:mt-20">
       <div className="-my-4 flex justify-center gap-5 overflow-hidden py-4 sm:gap-8">
-        {[image1, image2, image3, image4].map((image, imageIndex) => (
+        {[image1, image2, image3, image4, image5].map((image, imageIndex) => (
           <div
             key={image.src}
             className={clsx(
