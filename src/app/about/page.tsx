d03b6e7b4fe174c498b9bc65137e630396dbc031
qetzing <general@qetz.de>
2024-02-02 20:58:36 +0100

    Fix email link

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -91,7 +91,7 @@ export default function About() {
               Follow on LinkedIn
             </SocialLink>
             <SocialLink
-              href="mailto:spencer@planetaria.tech"
+              href="mailto:contact@derszteler.de"
               icon={EnvelopeIcon}
               className="mt-8 border-t border-zinc-100 pt-8 dark:border-zinc-700/40"
             >
