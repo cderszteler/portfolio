2b15bb24642c4009fa7839933413f0b72c531a0a
qetzing <general@qetz.de>
2024-02-27 21:40:57 +0100

    Fix and internationalize not-found page

--- src/app/[locale]/[...notFound]/page.tsx
+++ src/app/[locale]/[...notFound]/page.tsx
@@ -1,8 +1,15 @@
-import { Container } from '@/components/Container'
+import {Container} from '@/components/Container'
 import {Button} from "@/components/Button";
+import initTranslations from "@/app/i18n";
 
-// TODO: Fix not found
-export default function NotFound() {
+const i18nNamespaces = ['index'];
+
+export default async function NotFound({ params: { locale } }:
+{
+  params: { locale: string }
+}) {
+  const { t } = await initTranslations(locale, i18nNamespaces);
+
   return (
     <Container className="flex h-full items-center pt-16 sm:pt-32">
       <div className="flex flex-col items-center">
@@ -10,13 +17,13 @@ export default function NotFound() {
           404
         </p>
         <h1 className="mt-4 text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100">
-          Page not found
+          {t("notFound.title")}
         </h1>
         <p className="mt-4 text-base text-zinc-600 dark:text-zinc-400">
-          Sorry, we couldn’t find the page you’re looking for.
+          {t("notFound.description")}
         </p>
         <Button href="/" variant="secondary" className="mt-4">
-          Go back home
+          {t("notFound.button")}
         </Button>
       </div>
     </Container>
